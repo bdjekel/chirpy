78ab9c63150994e7c2b9a73f6d09eea072476b65
bdjekel <bdjekel@gmail.com>
2025-03-27 15:43:59 -0500

    plugged into ethernet and now I can submit tests with no issue...time to wire up som cat6 jacks.

--- main.go
+++ main.go
@@ -6,13 +6,21 @@ import (
 )
 
 func main() {
+
 	mux := http.NewServeMux()
 
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080"}
 
-	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
