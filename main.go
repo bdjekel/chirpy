a44632de2a25411f163470cc04f9318b8bd6f841
bdjekel <bdjekel@gmail.com>
2025-03-27 22:40:02 -0500

    Added middleware wrapper to count api hits on website. stateful handler. tests passed

--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 
 	mux := http.NewServeMux()
-
+	hitTracker := apiConfig{}
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080"}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	fileServerWithHitTracker := hitTracker.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", fileServerWithHitTracker)
+
 	mux.HandleFunc("/healthz", readinessHandler)
+	mux.HandleFunc("/metrics", hitTracker.hitCountHandler)
+	mux.HandleFunc("/reset", hitTracker.hitResetHandler)
 
 	log.Fatal(server.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hitCountHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %v", hits)))
+}
+
+func (cfg *apiConfig) hitResetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
 }
 
 func readinessHandler(w http.ResponseWriter, r *http.Request) {
