ee585892133dfe48426a43c1ea970c3eb6f5be3a
bdjekel <bdjekel@gmail.com>
2025-04-03 22:05:56 -0500

    small save

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,6 +20,7 @@ func main() {
 	mux.Handle("/app/", fileServerWithHitTracker)
 
 	mux.HandleFunc("GET /api/healthz", readinessHandler)
+	mux.HandleFunc("POST /api/validate_chirp", chirpValidator)
 	mux.HandleFunc("GET /admin/metrics", hitTracker.hitCountHandler)
 	mux.HandleFunc("POST /admin/reset", hitTracker.hitResetHandler)
 
@@ -51,4 +53,46 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	r.Header.Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func chirpValidator(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type error struct {
+		Error string `json:"error"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		// How do I return the error correctly here? Should I return an error anywhere else? REad lesson and docs
+		w.WriteHeader(500) // confirm error code is correct
+	}
+
+	// How do I return struct here properly? Review lesson, read docs
+}
+
+func handler(w http.ResponseWriter, r *http.Request){
+    type parameters struct {
+        // these tags indicate how the keys in the JSON should be mapped to the struct fields
+        // the struct fields must be exported (start with a capital letter) if you want them parsed
+        Name string `json:"name"`
+        Age int `json:"age"`
+    }
+
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+        // an error will be thrown if the JSON is invalid or has the wrong types
+        // any missing fields will simply have their values in the struct set to their zero value
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+    }
+    // params is a struct with data populated successfully
+    // ...
 }
