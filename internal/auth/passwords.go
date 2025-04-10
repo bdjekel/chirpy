d689f66c0b78ebd5986038b03e1af602d7b37480
bdjekel <bdjekel@gmail.com>
2025-04-10 12:21:38 -0500

    created jwt.go file with MakeJWT and ValidateJWT functions. No red squigglies. NEXT STEP: create unit tests for these functions.

--- internal/auth/passwords.go
+++ internal/auth/passwords.go
@@ -1,16 +1,19 @@
 package auth
 
 import (
+	"log"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) (string, error) {
 	hashed_password, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
+		log.Println("Password hashing failed.")
 		return "", err
-	} else {
-		return string(hashed_password), nil
 	}
+	
+	return string(hashed_password), nil
 }
 
 func CheckPasswordHash(hash, password string) error {
