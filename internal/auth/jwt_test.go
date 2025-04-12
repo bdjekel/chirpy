444273096d7b439784ab0fa800049fe7e4ca7288
bdjekel <bdjekel@gmail.com>
2025-04-11 21:01:00 -0500

    started jwt_test.go, but did not get very far.

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -1,4 +1,22 @@
 package auth
 
-func testMakeJWT() {
+import (
+	"testing"
+
+	"github.com/bdjekel/chirpy/internal/auth"
+)
+
+func TestMakeJWT(t *testing.T) {
+	auth.MakeJWT()
 }
+
+idCount := 15
+userIDs := []string{}
+
+for i:=0; i < idCount; i++ {
+	newID := gen_random_uuid()
+	userIDs = append(userIDs, newID)
+}
+
+secretToken := "ThIsIsAsEcReTtOkEn9876"
+expiresIn := rand.Intn(3600) * time.Second // 
